3d74118b250fc9ab35ef1cbcea933212a765acbf
Julian Herrera <julianeherreraok@gmail.com>
2022-11-08 16:07:06 -0300

    commit to save

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import React, { FC } from 'react'
+import { NavBar, SideMenu } from '../ui'
+
+
+interface Props {
+    title: string,
+    pageDescription: string,
+    imageFullUrl?: string,
+    children?: JSX.Element | JSX.Element[];
+}
+
+export const ShopLayout: FC<Props> = ({ children, title, pageDescription, imageFullUrl }) => {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+                <meta name='description' content={pageDescription} />
+                <meta name='og:title' content={title} />
+                <meta name='og:description' content={pageDescription} />
+                {
+                    imageFullUrl && (<meta name='og:image' content={imageFullUrl} />)
+                }
+
+            </Head>
+            <nav> <NavBar /></nav>
+            <SideMenu />
+            <main style={{ margin: '80px auto', maxWidth: '1440px', padding: '0px 30px' }}>
+                {children}
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    )
+}
