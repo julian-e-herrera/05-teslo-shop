73f712d1465fddf72ac3d67142329fa8c696c9ef
Julian Herrera <julianeherreraok@gmail.com>
2022-12-05 15:58:05 -0300

    middleware

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import { withAuth } from 'next-auth/middleware';
+ 
+export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    function middleware(req) {
+        // console.log(req.nextauth);
+        const { role } = req.nextauth.token?.user as any;
+        const validRoles = ['admin', 'super-user', 'seo'];
+ 
+        if (!validRoles.includes(role)) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+    },
+    {
+        callbacks: {
+            authorized: ({ token }) => !!token
+        }
+    }
+);
+ 
+export const config = {
+    matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*']
+};
